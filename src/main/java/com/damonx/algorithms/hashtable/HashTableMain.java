d5fa1ad40415db309e656a49ad62f9f625786d78
Damon Xu <damon.xu@spark.co.nz>
2025-10-30 22:13:11 +1300

    Hashtable

---
+++ src/main/java/com/damonx/algorithms/hashtable/HashTableMain.java
@@ -0,0 +1,11 @@
+package com.damonx.algorithms.hashtable;
+
+public class HashTableMain
+{
+    public static void main(String[] args)
+    {
+        final HashTable hashTable = new HashTable();
+        hashTable.printTable();
+
+    }
+}
